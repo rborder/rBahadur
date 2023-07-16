65d8b936259abf447cac89f5d45a3a4fa4c133d6
rborder <ribo7412@colorado.edu>
2023-07-16 13:23:11 -0700

    allow user-specified allele frequencies

--- R/am_simulate.R
+++ R/am_simulate.R
@@ -4,7 +4,8 @@
 #' @param r cross-mate phenotypic correlation
 #' @param m number of biallelic causal variants
 #' @param n sample size
-#' @param min_MAF minimum minor allele frequency for causal variants
+#' @param afs (optional). Allele frequencies to use. If not provided, `m` will be drawn uniformly from the interval [`maf`, 1-`maf`]
+#' @param min_MAF (optional) minimum minor allele frequency for causal variants. Ignored if if `afs` is not NULL. Defaults to 0.1
 #' @param haplotypes logical. If TRUE, includes (phased) haploid genotypes in output. Defaults to FALSE
 #'
 #' @return A list including the following objects:
@@ -30,11 +31,17 @@
 #' (emp_h2 <- var(sim_dat$g)/var(sim_dat$y))
 #' h2_eq(r, h2_0)
 
-am_simulate <- function(h2_0, r, m, n, min_MAF=.1, haplotypes=FALSE) {
+am_simulate <- function(h2_0, r, m, n, afs=NULL, min_MAF=.1, haplotypes=FALSE) {
   ## draw standardized diploid allele substitution effects
   beta <- scale(rnorm(m))*sqrt(h2_0 / m)
-  ## draw allele frequencies
-  AF <- runif(m, min_MAF, 1 - min_MAF)
+  ## draw allele frequencies if necessary
+  if (is.null(afs)) {
+    AF <- runif(m, min_MAF, 1 - min_MAF)
+  } else if (length(afs) != m) {
+    stop("`afs` must have length `m`")
+  } else {
+    AF <- afs
+  }
   ## compute unstandardized effects
   beta_unscaled <- beta/sqrt(2*AF*(1-AF))
   ## generate corresponding haploid quantities
